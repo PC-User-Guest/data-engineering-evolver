9e18bff618fa4e8bf2aa3ae7a95ff027a2c5a510
GitHub Action <action@github.com>
2026-03-14 02:05:08 +0000

    feat: Add sample data generator script

--- data_pipelines/etl.py
+++ data_pipelines/etl.py
@@ -40,3 +40,9 @@ if __name__ == "__main__":
 # added transformation 'filter low quantity'
 # added transformation 'log revenue'
 # added transformation 'add discount'
+<<<<<<< Updated upstream
+=======
+
+# added transformation 'filter low quantity'%
+# added transformation 'log revenue'%
+>>>>>>> Stashed changes
